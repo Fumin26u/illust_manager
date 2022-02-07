0f00978592fbf1fd2c907405ab9fa9428eecd8af
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-02-07 14:31:45 +0900

    ブックマークファイルの作成1

--- refimgs.php
+++ refimgs.php
@@ -1,9 +1,2 @@
 <?php
 $home = "./";
-// require_once('commonlib.php');
-
-// Pythonを呼び出す
-// コマンドライン入力
-$cmd = "python ./refimgs.py";
-exec($cmd, $result);
-var_dump($result);
--- refimgs.py
+++ refimgs.py
@@ -1,11 +1,59 @@
-# テスト用: Excelファイルに画像URL一覧を添付する用
-import openpyxl
 # タイマー: 同時複数リクエストを制限
 import time
 # ドライバ: スクレイピング用のドライバ
 from selenium import webdriver
 # urllib: URL引数を読み込む
 import urllib.parse
+# pixivpy: pixivからデータを抽出するAPI
+from pixivpy3 import *
 
-test = [10, 100, 1000]
-print(test)
+# import APIkey
+import sys
+import os
+sys.path.append(os.path.abspath('..'))
+import apikey_pi
+
+# Auth接続
+api = AppPixivAPI()
+api.auth(refresh_token = apikey_pi.REFRESH_TOKEN)
+aapi = AppPixivAPI()
+aapi.auth(refresh_token = apikey_pi.REFRESH_TOKEN)
+
+# PHP側に送信する用の配列
+res = []
+
+# データを一時的に保存するキュー
+queue = {}
+# ブックマークを取得
+bookmarks = aapi.user_bookmarks_illust(78079062, 'public')
+# print(bookmarks['next_url'])
+
+# 画像をurl配列に挿入
+for i, b in enumerate(bookmarks['illusts']):
+    res.insert(i, [])
+     
+    # メタ情報の追加
+    queue['post_time'] = b['create_date']
+    # queue['user_id'] = b['user']['id']
+    queue['user'] = b['user']['name']
+    queue['title'] = b['title']
+    queue['text'] = b['caption']
+    queue['images'] = []
+    queue['url'] = 'https://www.pixiv.net/artworks/' + str(b['id'])
+
+    # 画像の挿入
+    # 画像が1枚の場合
+    if len(b['meta_pages']) == 0:
+        queue['images'].append(b['image_urls']['large'])
+    # 画像が複数枚の場合
+    else:
+        for m in b['meta_pages']:
+            queue['images'].append(m['image_urls']['original'])      
+
+    # キューを結果に挿入
+    res.append(queue.copy())
+
+# print(res)
+
+
+
