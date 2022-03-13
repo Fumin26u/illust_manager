05aa6d936318e1c6885a14110367748a75ce709b
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-03-13 20:42:33 +0900

    表示部分を作成

--- public/u/profile.php
+++ public/u/profile.php
@@ -10,12 +10,44 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
+// ユーザー情報の初期化
+$user_info = [
+    'user_name' => [
+        'title' => 'ユーザー名',
+        'value' => ''
+    ],
+    'email' => [
+        'title' => 'メールアドレス',
+        'value' => ''
+    ],
+    'created_at' => [
+        'title' => 'アカウント作成時刻',
+        'value' => ''
+    ],
+    'premium' => [
+        'title' => '会員登録状況',
+        'value' => ''
+    ],
+    'dl_count' => [
+        'title' => 'ダウンロード回数',
+        'value' => ''
+    ],
+    'images_count' => [
+        'title' => '保存した画像の総数',
+        'value' => ''
+    ],
+    'latest_time' => [
+        'title' => '最新の利用時刻',
+        'value' => ''
+    ],
+];
+
 try {
     $pdo = dbConnect();
 
     // 表示するユーザーデータを読み込む
     $sql = <<<SQL
-SELECT user_name, email, premium, dl_count, images_count, created_at, used_time.latest_time FROM user
+SELECT user_name, email, created_at, premium, dl_count, images_count, used_time.latest_time FROM user
 INNER JOIN used_time ON used_time.user_id = user.user_id
 WHERE user.user_id = :user_id 
 AND used_time.sns_type = 'T' 
@@ -23,23 +55,28 @@ SQL;
     $st = $pdo->prepare($sql);
     $st->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $st->execute();
-    $user_info = $st->fetch(PDO::FETCH_ASSOC);
+    $rows = $st->fetch(PDO::FETCH_ASSOC);
     // プレミアム会員かどうか
-    switch ($user_info['premium']) {
+    switch ($rows['premium']) {
         case 'P':
-            $user_info['premium'] = 'プレミアム会員';
+            $rows['premium'] = 'プレミアム会員';
             break;
         default:
-            $user_info['premium'] = '一般会員';
+            $rows['premium'] = '一般会員';
             break;
     }
     // DL回数と保存した画像の枚数はNULLの場合0に置き換える
-    $user_info['dl_count'] = is_null($user_info['dl_count']) ? 0 : $user_info['dl_count'];
-    $user_info['images_count'] = is_null($user_info['images_count']) ? 0 : $user_info['images_count'];
+    $rows['dl_count'] = is_null($rows['dl_count']) ? 0 . '回': $rows['dl_count'] . '回';
+    $rows['images_count'] = is_null($rows['images_count']) ? 0 . '枚' : $rows['images_count'] . '枚';
     // 日付関連の置き換え
-    // $user_info['created_at'] = date('Y年m月d日 h時i分', $user_info['created_at']);
-    // $user_info['latest_time'] = date('Y年m月d日 h時i分', $user_info['latest_time']);
+    // $rows['created_at'] = date('Y年m月d日 h時i分', $rows['created_at']);
+    // $rows['latest_time'] = date('Y年m月d日 h時i分', $rows['latest_time']);
 
+    // ユーザー情報に挿入
+    foreach ($user_info as $k => $u) {
+        $user_info[$k]['value'] = $rows[$k];
+    }
+ 
 } catch (PDOException $e) {
     echo 'データベース接続に失敗しました';
     if (DEBUG) {
@@ -50,7 +87,7 @@ SQL;
 $url = 'https://imagedler.com/u/signup.php?t=';
 
 // デスクリプション
-$title = $user_info['user_name'] . 'さんのユーザー情報';
+$title = $user_info['user_name']['value'] . 'さんのユーザー情報';
 
 $description = "Twitterで自分が「いいね」をした画像を一括ダウンロードできるツールです。ユーザー登録を行うことにより、更に手軽にダウンロードを行うことができます。";
 
@@ -78,7 +115,15 @@ $canonical = "https://imagedler.com/u/profile.php";
 <body>
 <?php include_once($home . '../header.php') ?>
 <main>
-
+    <h2><?= $user_info['user_name']['value'] ?>さんのユーザー情報</h2>
+    <dl class="form_list">
+        <?php foreach ($user_info as $u) { ?>
+            <div>
+                <dt><?= $u['title'] ?></dt>
+                <dd><?= $u['value'] ?></dd>
+            </div>
+        <?php } ?>
+    </dl>
 </main>
 <?php include_once($home . '../footer.php') ?>
 </body>
