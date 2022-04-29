ea501ca8393827f30f979c9865299b38913e5470
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-04-29 20:46:14 +0900

    a

---
+++ Database/Connection/SystemConfig.php
@@ -0,0 +1,16 @@
+<?php 
+// namespace Database\Connection;
+
+// class DBInfo {
+//     protected const DSN = 'mysql:host=localhost;dbname=twimagedler;charset=utf8mb4;';
+//     protected const DBUSER = 'root';
+//     protected const DBPASS = '';
+    
+//     protected const DEBUG = true;
+// }
+
+const DSN = 'mysql:host=localhost;dbname=twimagedler;charset=utf8mb4;';
+const DBUSER = 'root';
+const DBPASS = '';
+
+const DEBUG = true;
