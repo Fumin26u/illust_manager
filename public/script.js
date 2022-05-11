d94c33bbf751245af7ea44d35a2ea5f95118f37f
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-05-11 13:17:35 +0900

    期間指定の表示非表示のトグル作成3

--- public/script.js
+++ public/script.js
@@ -7,11 +7,22 @@
 
     // ツイート取得の場合の期間指定の表示
     const objects = document.getElementsByName("object");
+    const tweets = document.getElementById("tweets");
     const u = document.getElementById("using_term_area");
-    for (object of objects) {
-        object.addEventListener('change', () => {
-            console.log(object);
+    for (let i = 0; i < objects.length; i++) {
+        objects[i].addEventListener('change', () => {
+            if (tweets.checked) {
+                u.style.display = "block";
+            } else {
+                u.style.display = "none";
+            }
         });
     }
-
+    window.onload = () => {
+        if (tweets.checked) {
+            u.style.display = "block";
+        } else {
+            u.style.display = "none";
+        }
+    }
 }
