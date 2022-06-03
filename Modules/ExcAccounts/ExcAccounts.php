8a13bcee604b5bd2449b74fcd2479ad98a12b72e
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-06-03 21:43:55 +0900

    除外ユーザー設定用のクラス作成

---
+++ Modules/ExcAccounts/ExcAccounts.php
@@ -0,0 +1,19 @@
+<?php
+namespace Modules\ExcAccounts;
+
+class ExcAccounts {
+
+    private array $users;
+
+    public function __construct(array $tweet_users) {
+        
+        $this->users = $tweet_users;
+
+    }
+
+    public static function showUsers() {
+
+        
+
+    }
+}
